682cce6aee697931b888c464a1ccee7f2503b68b
Paulo Alexandre <pauloaleexandre@gmail.com>
2024-04-01 17:12:02 -0300

    File created

--- Schema/faqschema.js
+++ Schema/faqschema.js
@@ -1,1 +1,42 @@
-document.addEventListener('DOMContentLoaded',()=>{const e={"@context":"http://schema.org","@type":"FAQPage",mainEntity:[]},t=document.querySelectorAll('.n-faq-questions-div .o-faq-item-wrapper');t.forEach(t=>{const n=t.querySelector('h3'),o=n?n.innerText.trim():'',c=t.querySelector('p'),r=c?c.innerText.trim():'';o&&r&&e.mainEntity.push({"@type":"Question",name:o,acceptedAnswer:{"@type":"Answer",text:r}})}),e.mainEntity.length>0&&(t=document.createElement('script'),t.type='application/ld+json',t.textContent=JSON.stringify(e),document.head.appendChild(t))});
+document.addEventListener("DOMContentLoaded", () => {
+  const faqSchema = {
+    "@context": "http://schema.org",
+    "@type": "FAQPage",
+    mainEntity: [],
+  };
+
+  // Seleciona todos os itens do FAQ
+  const faqItems = document.querySelectorAll(
+    ".n-faq-questions-div .o-faq-item-wrapper"
+  );
+
+  faqItems.forEach((item) => {
+    // Captura o texto do primeiro elemento 'h3' encontrado dentro do item do FAQ para a pergunta
+    const questionElement = item.querySelector("h3");
+    const question = questionElement ? questionElement.innerText.trim() : "";
+
+    // Prepara-se para capturar a resposta como 'p', assumindo futura mudança
+    const answerElement = item.querySelector("p"); 
+    const answer = answerElement ? answerElement.innerText.trim() : "";
+
+    // Adiciona a pergunta e resposta ao objeto schema, se ambos forem encontrados
+    if (question && answer) {
+      faqSchema.mainEntity.push({
+        "@type": "Question",
+        name: question,
+        acceptedAnswer: {
+          "@type": "Answer",
+          text: answer,
+        },
+      });
+    }
+  });
+
+  // Insere o schema de FAQ no <head> da página, se algum item foi adicionado
+  if (faqSchema.mainEntity.length > 0) {
+    const script = document.createElement("script");
+    script.type = "application/ld+json";
+    script.textContent = JSON.stringify(faqSchema);
+    document.head.appendChild(script);
+  }
+});
